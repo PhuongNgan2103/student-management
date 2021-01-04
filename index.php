bf36dc6457e71824485549c2d5b41281af6be452
PhuongNgan2103 <nguyenphuongngan2103@gmail.com>
2021-01-04 10:55:52 +0700

    style th

--- index.php
+++ index.php
@@ -6,9 +6,18 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="Styles.css">
 </head>
 <body>
-
+<table>
+    <tr>
+        <th>STT</th>
+        <th>Họ và tên</th>
+        <th>Ngày sinh</th>
+        <th>Giới tính</th>
+        <th>Ghi chú</th>
+    </tr>
+</table>
 </body>
 </html>
 <?php
