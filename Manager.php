23e30390e0423cd83df79f060896f89bcd7fe82c
PhuongNgan2103 <nguyenphuongngan2103@gmail.com>
2021-01-04 20:24:22 +0700

    add function saveData

--- Manager.php
+++ Manager.php
@@ -1,5 +1,6 @@
 <?php
 class Manager{
+    const FILE_DATA_NAME = 'data.json';
     public static $students = [];
 
     public function add($student)
@@ -32,8 +33,18 @@ class Manager{
         rsort(self::$students);
     }
 
-    public function search($student)
+    public function search()
     {
 
+    }
+
+    public function saveData()
+    {
+        $data = [];
+        foreach (self::$students as $student){
+            $data = $student->toArray();
+        }
+        $dataJson = json_encode($data);
+        file_put_contents(self::FILE_DATA_NAME,$dataJson);
     }
 }
